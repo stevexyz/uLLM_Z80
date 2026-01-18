2c7d551a4ad3276f57b232c348195befe49d8807
steve <5434279+stevexyz@users.noreply.github.com>
2026-01-19 00:30:37 +0100

    utility

--- train_and_run_chat.sh
+++ train_and_run_chat.sh
@@ -2,9 +2,14 @@
 
 # to activate torch python environment ". torchrun/bin/activate"
 
+if [ "$1" == "--newrun" ]
+  then
+    rm command_model_autoreg.pt
+fi
+
 if [ "$1" != "--justrun" ]
   then
     cat examples/smschat/training-data.txt | ./feedme.py --epochs 300 --chat
 fi
 ./buildz80com.py -m command_model_autoreg.pt -o CHAT.COM
-./buildfastz80com.py -m command_model_autoreg.pt -o CHATF.COM
+#./buildfastz80com.py -m command_model_autoreg.pt -o CHATF.COM
